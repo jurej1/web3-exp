994f0a6b62e225bed172bd4a54bc1216f125e166
Jure Jures <jure.04jj@gmail.com>
2025-05-31 14:58:13 +0200

    adding button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return null;
+  return <h1>Home Page</h1>;
 }
---
+++ src/app/swap/page.tsx
@@ -0,0 +1,3 @@
+export default function SwapPage() {
+  return <h1>Swap Page</h1>;
+}
